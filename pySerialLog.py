cc987dfc5eb8bf3dafed79bdb0be8cd025bc55a6
msfassarella <33352657+msfassarella@users.noreply.github.com>
2019-03-25 20:39:57 +0000

    Update pySerialLog.py
    Procura a porta serial USB existente e abre a que estiver disponivel.
    Enquanto nao houver porta serial o script fica em modo de espera.
    Ideal para inicializar o raspberry pi chamando o script em /etc/rc.local e, mesmo sem plugar a serial, 
    o script consegue inicializar sem erro e fica aguardando a USB aparecer.

--- pySerialLog.py
+++ pySerialLog.py
@@ -1,17 +1,36 @@
 import time
 import serial
+import os
+
 
 #ser = serial.Serial(port='/dev/ttyUSB0',baudrate=230400,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=10)
 portaSerial = False
 while (portaSerial == False):
+   tmp = os.popen('ls /dev/ttyUSB*').read()
+   print (tmp)
+   print('\r\n')
+   lentmp =  len(tmp)
+   tmp2 = tmp[:lentmp-2]
+   print (tmp2)
+   if tmp2 == '/dev/ttyUSB':
+      portaSerial = True
+   else:
+      time.sleep(5)       
+    #print('errado')
+tmp = tmp[:12]
+print('Porta encontrada\n')
+time.sleep(10)
+
+portaSerial = False
+while (portaSerial == False):
    try:
-      ser = serial.Serial(port='/dev/ttyUSB0',baudrate=230400,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=10)
+      print('Abrindo: ' + tmp) 
+      ser = serial.Serial(port=tmp,baudrate=230400,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=10)
       portaSerial = True
    except:
-      time.sleep(1)
-      print ('abrindo ...\r\n')
+      time.sleep(2)
+      #print ('abrindo ...\r\n')
         
-# 
 arquivo = open('/home/pi/Desktop/arq01.txt','a')
 while True:
    for i in range(10):
